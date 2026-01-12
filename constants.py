1e33ec3f8dfa2a2638985379488581536ba7b3e7
Spicyminion <1legoshark@gmail.com>
2026-01-11 22:11:10 -0500

    Adding stuff

--- constants.py
+++ constants.py
@@ -1,5 +1,18 @@
 import pygame
+from enum import Enum
 
+# TILE TYPES
+
+class TileType(Enum):
+    BLANK = 0
+    BASE = 1
+    SPAWN_CIRCLE = 2
+    LOCK_SPACE = 3
+    HOUSE = 4
+    MALL = 5
+
+
+# DIMENSIONS FOR BOARD AND SCREEN
 SCREEN_WIDTH, SCREEN_HEIGHT = 1800, 900
 
 SCALE_FACTOR = 4.0
@@ -9,4 +22,32 @@ DIMENSION = 7
 
 INITIAL_OFFSET_X = SCREEN_WIDTH / 2 - HALF_WIDTH
 INITIAL_OFFSET_Y = (SCREEN_HEIGHT / 2) - (HALF_HEIGHT * 2 * DIMENSION / 2)  # first div brings to mid, 2nd moves up height of half of board
+
+# PNG ASSETS
+BLOCK = pygame.image.load('grass_block.png')
+CLICKED_BLOCK = pygame.image.load('clicked_grass_block.png')
+COLOR_KEY = (0,0,0)
+w, h = BLOCK.get_size()
+
+BLOCK.set_colorkey(COLOR_KEY)
+CLICKED_BLOCK.set_colorkey(COLOR_KEY)
+
+BLOCK = pygame.transform.scale(BLOCK, (int(w * SCALE_FACTOR), int(h * SCALE_FACTOR)))
+CLICKED_BLOCK = pygame.transform.scale(CLICKED_BLOCK, (int(w * SCALE_FACTOR), int(h * SCALE_FACTOR)))
+
+'''
+def load_assets():
+    global BLOCK, CLICKED_BLOCK
+    COLOR_KEY = (0,0,0)
+    block = pygame.image.load('grass_block.png').convert()
+    clicked_block = pygame.image.load('clicked_grass_block.png').convert()
+    w, h = block.get_size()
+
+    block.set_colorkey(COLOR_KEY)
+    clicked_block.set_colorkey(COLOR_KEY)
+
+    BLOCK = pygame.transform.scale(block,(int(w * SCALE_FACTOR), int(h * SCALE_FACTOR)))
+    CLICKED_BLOCK = pygame.transform.scale(clicked_block,(int(w * SCALE_FACTOR), int(h * SCALE_FACTOR)))
+'''
+
 
--- tile.py
+++ tile.py
@@ -1,17 +1,19 @@
 import pygame
+from constants import SCALE_FACTOR, HALF_HEIGHT, HALF_WIDTH, BLOCK, CLICKED_BLOCK, TileType, INITIAL_OFFSET_Y, INITIAL_OFFSET_X
+#import constants
+
 class Tile:
 
-    def __init__(self, x, y, type, window):
+    def __init__(self, type, x, y):
         self.x = x
         self.y = y
         self.type = type
-        self.window = window
         self.characters = []
 
-
-    def draw(self):
-        if type == 0:
-            window.bliu
-
+    def draw(self, window):
+        if self.type == TileType.BLANK:
+            window.blit(BLOCK, (self.x, self.y))
+        else:
+            window.blit(CLICKED_BLOCK, (self.x, self.y))
 
 
